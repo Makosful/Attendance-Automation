37f73afcc2335158d4b86e88c49572d9da80f6fb
Axl <makosful@gmail.com>
2018-02-01 11:17:52 +0100

    Initial commit

---
+++ src/attendance/automation/gui/model/Model.java
@@ -0,0 +1,33 @@
+package attendance.automation.gui.model;
+
+/**
+ * This class will cache all data for the application while acting as the sole
+ * point of connection to the BL Layer
+ *
+ * @author Axl
+ */
+public class Model
+{
+
+    // Singleton instance of the model
+    private static final Model INSTANCE = new Model();
+
+    /**
+     * Singleton constructor. Prevents new instances of this class being made
+     * outside of this class
+     */
+    private Model()
+    {
+        // Acts as a normal constructor otherwise
+    }
+
+    /**
+     * Gets the single instance of the Model class
+     *
+     * @return The Model class
+     */
+    public static Model getInstance()
+    {
+        return INSTANCE;
+    }
+}
