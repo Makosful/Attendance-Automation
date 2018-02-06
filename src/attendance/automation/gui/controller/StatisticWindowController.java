719d1fdb5dd7223401675e491e45bc0ca28d4529
Axl <makosful@gmail.com>
2018-02-06 11:55:48 +0100

    Swapped out images with charts

--- src/attendance/automation/gui/controller/StatisticWindowController.java
+++ src/attendance/automation/gui/controller/StatisticWindowController.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package attendance.automation.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 
 /**
  * FXML Controller class
@@ -17,8 +17,24 @@ import javafx.fxml.Initializable;
 public class StatisticWindowController implements Initializable
 {
 
+    @FXML
+    private DatePicker dateFrom;
+    @FXML
+    private DatePicker dateTo;
+    @FXML
+    private Button btnSetDateStart;
+    @FXML
+    private Button btnSetDateToday;
+    @FXML
+    private PieChart chrtTotalAttendance;
+    @FXML
+    private BarChart<?, ?> chrtClassAttendance;
+
     /**
      * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb)
--- src/attendance/automation/gui/controller/StudentScreenController.java
+++ src/attendance/automation/gui/controller/StudentScreenController.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package attendance.automation.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 
 /**
  * FXML Controller class
@@ -17,8 +14,14 @@ import javafx.fxml.Initializable;
 public class StudentScreenController implements Initializable
 {
 
+    @FXML
+    private PieChart chrtStatistics;
+
     /**
      * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb)
