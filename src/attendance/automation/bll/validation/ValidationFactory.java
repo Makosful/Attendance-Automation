5ef4b47e7ac32630bef10e89760b1da55809f88b
davidmsf <davidmsf@gmail.com>
2018-03-21 22:21:23 +0100

    valid factory

---
+++ src/attendance/automation/bll/validation/ValidationFactory.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendance.automation.bll.validation;
+
+/**
+ *
+ * @author B
+ */
+public class ValidationFactory {
+    
+}
