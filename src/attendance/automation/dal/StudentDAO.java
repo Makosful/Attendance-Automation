42bd27ee397bccfa89400b66217a97a194acc337
dankphetamine <asge0907@easv365.dk>
2018-04-03 17:40:49 +0200

    sDAO update

--- src/attendance/automation/dal/StudentDAO.java
+++ src/attendance/automation/dal/StudentDAO.java
@@ -8,7 +8,9 @@ package attendance.automation.dal;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -16,36 +18,34 @@ import java.util.Date;
  * @author B
  */
 public class StudentDAO {
+
     private DataBaseConnector db;
-    
-    public StudentDAO()
-    {
+
+    public StudentDAO() {
         db = new DataBaseConnector();
     }
+
     /**
      * Registers whether the student is in school or not.
+     *
      * @param userID
      * @param attendance
      * @throws SQLServerException
-     * @throws SQLException 
+     * @throws SQLException
      */
-    public void registerAttendance(int userID, boolean attendance) throws SQLServerException, SQLException
-    {
-       try(Connection con = db.getConnection())
-       {
-           String sql = "INSERT INTO StudentAttendance VALUES(?, ?, getDate())";         
-           PreparedStatement pstmt = con.prepareStatement(sql);           
-           pstmt.setInt(1, userID);
-           pstmt.setBoolean(2, attendance);
-           
-           pstmt.executeUpdate();
-       }
+    public void registerAttendance(int userID, boolean attendance) throws SQLServerException, SQLException {
+        try (Connection con = db.getConnection()) {
+            String sql = "INSERT INTO StudentAttendance VALUES(?, ?, getDate())";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, userID);
+            pstmt.setBoolean(2, attendance);
+
+            pstmt.executeUpdate();
+        }
     }
-    
-    public void sendAttendanceChange(int studentID, int classID, String message, Date date) throws SQLServerException, SQLException
-    {
-        try(Connection con = db.getConnection())
-        {
+
+    public void sendAttendanceChange(int studentID, int classID, String message, Date date) throws SQLServerException, SQLException {
+        try (Connection con = db.getConnection()) {
             String sql = "INSERT INTO AttendanceChangeRequest VALUES(?, ?, ?, ?)";
             PreparedStatement pstmt = con.prepareStatement(sql);
             pstmt.setInt(1, studentID);
@@ -55,11 +55,27 @@ public class StudentDAO {
             pstmt.executeUpdate();
         }
     }
-    
-    public void changeStudentAttendance()
-    {
-        
+
+    public void changeStudentAttendance() {
+
     }
 
-    
+    public ArrayList<Boolean> registerAverageAttendance(int userID) throws SQLServerException, SQLException {
+        ArrayList<Boolean> avgList = new ArrayList<>();
+
+        try (Connection con = db.getConnection()) {
+
+            String sql = "SELECT * FROM StudentAttendance WHERE UserID = ?";
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, userID);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                avgList.add(rs.getBoolean("Attended"));
+            }
+        } catch (Exception e) {
+        }
+        return avgList;
+    }
 }
