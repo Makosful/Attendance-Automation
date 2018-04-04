44224f2020f9d2adc7fdaf30ed9669c587ee7c74
Husso1997 <hussain1961@hotmail.com>
2018-04-04 11:02:49 +0200

    pusssssh

--- src/attendance/automation/dal/TeacherDAO.java
+++ src/attendance/automation/dal/TeacherDAO.java
@@ -5,10 +5,54 @@
  */
 package attendance.automation.dal;
 
+import attendance.automation.be.NotificationMessage;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author B
  */
-public class TeacherDAO {
+public class TeacherDAO 
+{
+    private DataBaseConnector dbConnector; 
     
+    public TeacherDAO()
+    {
+        dbConnector = new DataBaseConnector();
+    }
+    
+    
+    public List<NotificationMessage> allNotifications(int... classID) throws SQLServerException, SQLException 
+    {
+        List<NotificationMessage> allNotifications;
+
+        try (Connection con = dbConnector.getConnection()) 
+        {
+            allNotifications = new ArrayList();
+            String sql = "SELECT * FROM AttendanceChangeRequest";
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) 
+            {
+                int messageClassID = rs.getInt("ClassID");
+                for (int i = 0; i < classID.length; i++)
+                {
+                    if (messageClassID == classID[i]) 
+                    {
+                        int StudentID = rs.getInt("StudentID");
+                        String message = rs.getString("Message");
+                        NotificationMessage notificationMessage = new NotificationMessage(StudentID, messageClassID, message);
+                        allNotifications.add(notificationMessage);
+                    }
+                }
+            }
+        }
+        return allNotifications;
+    }
 }
