18d4dc278251c14332d908646c248c83770631ec
davidmsf <davidmsf@gmail.com>
2018-04-06 09:42:44 +0200

    imports

--- src/attendance/automation/dal/StudentDAO.java
+++ src/attendance/automation/dal/StudentDAO.java
@@ -9,20 +9,14 @@ import attendance.automation.be.Student;
 import attendance.automation.be.User;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
